24f8e063862bbb219122a460ecf67369a7846a46
unknown <5026201099@student.its.ac.id>
2024-06-30 12:07:47 +0700

    feat: fixing bugs on song screen

--- lib/self_management/music/screen/list_music_page.dart
+++ lib/self_management/music/screen/list_music_page.dart
@@ -47,154 +47,139 @@ class _ListMusicState extends State<ListMusic> {
               child: CircularProgressIndicator(color: Color(0xFF64CDC2)),
             );
           } else {
-          return Stack(
-            children: [
-              Container(
-                height: MediaQuery.of(context).size.height,
-                color: const Color(0xffF6F1F1),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 40),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 20,),
-                        child: 
-                          Text(
-                            'Musik',
-                            style: TextStyle(
-                              color: Color(0xff3D3D3D),
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                      ),
-                      const SizedBox(
-                        height: 2,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 14.0),
-                        child: Expanded(
-                          child: SingleChildScrollView(
-                            child: GridView.builder(
-                              physics: const NeverScrollableScrollPhysics(),
-                              shrinkWrap: true,
-                              gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 1,
-                                crossAxisSpacing: 0,
-                                mainAxisSpacing: 0,
-                                mainAxisExtent: 70,
-                              ),
-                              itemCount: playlist.length,
-                              itemBuilder: (context, index) {
-                                final Song song = playlist[index];
-                                return GestureDetector(
-                                  onTap: () => goToSong(index),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Row(
-                                        crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            (index + 1).toString(),
-                                            style: GoogleFonts.plusJakartaSans(
-                                                color: const Color(0xff7D7D7D),
-                                                fontSize: 16,
-                                                fontWeight: FontWeight.w400),
-                                          ),
-                                          const SizedBox(
-                                            width: 16,
-                                          ),
-                                          SizedBox(
-                                            width: 44,
-                                            height: 44,
-                                            child: song.imgUrl.startsWith('http') || song.imgUrl.startsWith('https')
-                                                ? Image.network(
-                                              song.imgUrl,
-                                              fit: BoxFit.cover,
-                                            )
-                                                : Image.asset(
-                                              song.imgUrl,
-                                              fit: BoxFit.cover,
-                                            ),
-                                          ),
-                                          const SizedBox(
-                                            width: 16,
-                                          ),
-                                          Column(
-                                            mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                            crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                song.titleSong,
-                                                style:
-                                                GoogleFonts.plusJakartaSans(
-                                                    color: const Color(
-                                                        0xff2F9296),
-                                                    fontSize: 16,
-                                                    fontWeight:
-                                                    FontWeight.w600),
-                                              ),
-                                              Text(
-                                                song.artistName,
-                                                style:
-                                                GoogleFonts.plusJakartaSans(
-                                                    color: const Color(
-                                                        0xff7D7D7D),
-                                                    fontSize: 16,
-                                                    fontWeight:
-                                                    FontWeight.w400),
-                                              ),
-                                            ],
-                                          ),
-                                        ],
-                                      ),
-                                      IconButton(
-                                        icon: song.isFav
-                                            ? const Icon(Icons.favorite)
-                                            : const Icon(Icons.favorite_border),
-                                        color: song.isFav
-                                            ? const Color(0xff2F9296)
-                                            : Colors.grey.shade700,
-                                        onPressed: (){
-                                          playListProvider.toggleFavorite(index);
-                                        },
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
+          return Container(
+              height: MediaQuery.of(context).size.height,
+              color: const Color(0xffF6F1F1),
+              child: Stack(
+                children: <Widget>[
+                  const Positioned(
+                      top: 40,
+                      left: 14,
+                      child: Text(
+                        'Musik',
+                        style: TextStyle(
+                          color: Color(0xff3D3D3D),
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
                         ),
-                      ),
-                    ],
+                      )
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Consumer<PlayListProvider>(
-                    builder: (context, value, child){
-                      if(value.currentSongIndex == null){
-                        return const SizedBox.shrink();
-                      }
-                      return const MiniPlayer();
-                    },
+                  Positioned(
+                      top: 100,
+                      left: 14,
+                      right: 14,
+                      child: SingleChildScrollView(
+                        child: GridView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 1,
+                              crossAxisSpacing: 0,
+                              mainAxisSpacing: 0,
+                              mainAxisExtent: 70,
+                            ),
+                            itemCount: playlist.length,
+                            itemBuilder: (context, index) {
+                              final Song song = playlist[index];
+                              return GestureDetector(
+                                onTap: () => goToSong(index),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    // konten sebelah kiri dalam row
+                                    Row(
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: <Widget>[
+                                        Text(
+                                          (index + 1).toString(),
+                                          style: GoogleFonts.plusJakartaSans(
+                                              color: const Color(0xff7D7D7D),
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.w400),
+                                        ),
+                                        const SizedBox(
+                                          width: 16,
+                                        ),
+                                        SizedBox(
+                                          width: 44,
+                                          height: 44,
+                                          child: song.imgUrl.startsWith('http') || song.imgUrl.startsWith('https')
+                                              ? Image.network(
+                                            song.imgUrl,
+                                            fit: BoxFit.cover,
+                                          )
+                                              : Image.asset(
+                                            song.imgUrl,
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                        const SizedBox(
+                                          width: 16,
+                                        ),
+                                        Column(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: <Widget>[
+                                            Text(
+                                              song.titleSong,
+                                              style:
+                                              GoogleFonts.plusJakartaSans(
+                                                  color: const Color(
+                                                      0xff2F9296),
+                                                  fontSize: 16,
+                                                  fontWeight:
+                                                  FontWeight.w600),
+                                            ),
+                                            Text(
+                                              song.artistName,
+                                              style:
+                                              GoogleFonts.plusJakartaSans(
+                                                  color: const Color(
+                                                      0xff7D7D7D),
+                                                  fontSize: 16,
+                                                  fontWeight:
+                                                  FontWeight.w400),
+                                            ),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                    IconButton(
+                                      padding: const EdgeInsets.only(top: 18.0),
+                                      icon: song.isFav
+                                          ? const Icon(Icons.favorite)
+                                          : const Icon(Icons.favorite_border),
+                                      color: song.isFav
+                                          ? const Color(0xff2F9296)
+                                          : Colors.grey.shade700,
+                                      onPressed: (){
+                                        playListProvider.toggleFavorite(index);
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              );
+                            }
+                        ),
+                      )
                   ),
-                ),
-              ),
-            ],
-          );
+                  Positioned(
+                      bottom: 0,
+                      left: 8,
+                      right: 8,
+                      child: Consumer<PlayListProvider>(
+                        builder: (context, value, child) {
+                          if(value.currentSongIndex == null){
+                            return const SizedBox.shrink();
+                          }
+                          return const MiniPlayer();
+                        },
+                      )
+                  )
+                ],
+              )
+            );
           }
         },
       ),
